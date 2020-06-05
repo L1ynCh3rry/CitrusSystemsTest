3ba9a936be118c446d5601ee2d2a1cb2d5d55bfb
Luka Cosic <luka1cosic67@gmail.com>
2020-06-05 18:37:59 +0200

    Finishing

--- js/javascript.js
+++ js/javascript.js
@@ -16,25 +16,29 @@ function mobileFunction() {
     document.getElementById("buttonMobile").classList.add('active');
 };
 
-/* Register */
+/* Email Register */
 function emailRegFunction() {
     var email, terms, regEmail, errors, correct, outcome;
     email = document.getElementById("emailEmail").value;
     terms = document.getElementById("checkterms");
 
+    /* Email checker */
     regEmail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
 
+    /* Arrays for errors and success */
     errors = new Array();
     correct = new Array();
 
+    /* Testing email */
     if (!regEmail.test(email)) {
-        errors.push("Email is incorrect");
+        errors.push("Your email address is incorrect");
     } else {
         correct.push("Email: " + email);
     }
+    /* Testing if terms is checked */
     if (terms.checked) {
 
-        correct.push("You accepted terms");
+        correct.push("You accepted the terms");
     } else {
         errors.push("You must accept our terms and conditions!");
     }
@@ -47,67 +51,75 @@ function emailRegFunction() {
             outcome += "<li style='color:#ff0000;'>" + errors[i] + "</li>";
         }
     }
-
     outcome += "</ul>";
-
     document.getElementById("error").style.display = "block";
     document.getElementById("error").innerHTML = outcome;
 
+    /* Starting 1st timer here */
     if (errors.length == 0) {
         setTimeout(loading, 200.0)
     }
 
+    /* Loading function and starting 2nd timer */
     function loading() {
         document.getElementById("success").classList.add('succesfully');
         document.getElementById('darkblock').classList.add('darker');
         setTimeout(loadingend, 3000.0)
-
     }
 
+    /* Final loading and ending of registration */
     function loadingend() {
         document.getElementById("success").classList.remove('succesfully');
         document.getElementById('darkblock').classList.remove('darker');
+        /* Results */
         if (errors.length == 0) {
             outcome = "<ul>";
             for (var i = 0; i < correct.length; i++) {
-                outcome += "<li style='color:#0fe300;'>" + correct[i] + "</li>";
+                outcome += "<li style='color:#137000;'>" + correct[i] + "</li>";
             }
             outcome += "</ul>";
 
+            /* Clearing and getting done */
             document.getElementById("error").style.display = "block";
+            document.getElementById("login").style.display = "block";
+            document.getElementById("error").style.top = "20%";
+            document.getElementById("blockemail").style.display = "none";
+            document.getElementById("blockmobile").style.display = "none";
             document.getElementById("error").innerHTML = outcome;
+            document.getElementById('buttonEmail').onclick = null;
+            document.getElementById('buttonMobile').onclick = null;
+            document.getElementById('buttonMobile').classList.add('disablehover');
         }
     }
-
-
-
 }
 
 
 
-/* Register */
+/* Mobile Register */
 function mobileRegFunction() {
     var mobile, terms, regMobile, errors, correct, outcome;
     mobile = document.getElementById("mobile").value;
     terms = document.getElementById("mcheckterms");
 
+    /* Mobile checker */
     regMobile = /^(\+\d{1,3}[- ]?)?\d{10}$/;
 
+    /* Arrays for errors and success */
     errors = new Array();
     correct = new Array();
 
+    /* Checking mobile number */
     if (!regMobile.test(mobile)) {
-        errors.push("Mobile phone is incorrect");
+        errors.push("Your mobile phone is incorrect, mobile should be exactly 10 numbers");
     } else {
         correct.push("Number: " + mobile);
     }
+    /* Checking if terms is checked */
     if (terms.checked) {
-
-        correct.push("You accepted terms");
+        correct.push("You accepted the terms");
     } else {
         errors.push("You must accept our terms and conditions!");
     }
-
 
     // FINALIZING
     outcome = "<ul>";
@@ -116,36 +128,45 @@ function mobileRegFunction() {
             outcome += "<li style='color:#ff0000;'>" + errors[i] + "</li>";
         }
     }
-
     outcome += "</ul>";
-
+    /* Displaying errors */
     document.getElementById("error").style.display = "block";
     document.getElementById("error").innerHTML = outcome;
 
+    /* Starting first timer here */
     if (errors.length == 0) {
         setTimeout(loading, 200.0)
     }
 
+    /* Ending function and 2nd timer */
     function loading() {
         document.getElementById("success").classList.add('succesfully');
         document.getElementById('darkblock').classList.add('darker');
         setTimeout(loadingend, 3000.0)
-
     }
 
+    /* Final function */
     function loadingend() {
         document.getElementById("success").classList.remove('succesfully');
         document.getElementById('darkblock').classList.remove('darker');
+        /* Results */
         if (errors.length == 0) {
             outcome = "<ul>";
             for (var i = 0; i < correct.length; i++) {
-                outcome += "<li style='color:#0fe300;'>" + correct[i] + "</li>";
+                outcome += "<li style='color:#137000;'>" + correct[i] + "</li>";
             }
             outcome += "</ul>";
 
+            /* Clearing and getting done */
             document.getElementById("error").style.display = "block";
+            document.getElementById("login").style.display = "block";
+            document.getElementById("error").style.top = "20%";
+            document.getElementById("blockemail").style.display = "none";
+            document.getElementById("blockmobile").style.display = "none";
             document.getElementById("error").innerHTML = outcome;
+            document.getElementById('buttonEmail').onclick = null;
+            document.getElementById('buttonMobile').onclick = null;
+            document.getElementById('buttonEmail').classList.add('disablehover');
         }
     }
-
 }
